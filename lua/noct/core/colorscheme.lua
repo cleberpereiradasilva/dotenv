e9bc6d2de4443656412245f75ccf7e7818bbd34c
Cleber Silva <cleber.pereira@pm.me>
2024-02-10 00:05:35 -0300

    reorganize folders

--- lua/noct/core/colorscheme.lua
+++
@@ -1,5 +0,0 @@
-local status, _ = pcall(vim.cmd,[[ colorscheme nightfly ]]) 
-if not status then
-  print("Colorsheme not found!!!")
-  return
-end
--- lua/noct/plugins/lsp/gitsigns.lua
+++
@@ -1,9 +0,0 @@
-local setup, gitsigns = pcall(require, "gitsigns")
-if not setup then
-	return
-end
-
-gitsigns.setup({
-	current_line_blame = true,
-	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-})
