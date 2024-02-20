9e1c7b9dccd2e1473aacc1f2786d76107d461ac9
Cleber Silva <noct@MacBook-Pro.local>
2024-02-20 07:19:17 -0300

    set gd and gv

--- .config/nvim/lua/noct/core/keymaps.lua
+++ .config/nvim/lua/noct/core/keymaps.lua
@@ -3,11 +3,11 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>nh", ":nohl<CR>")
-
 keymap.set("n", "x", '"_x') -- in NORMAL mode use x to delete single character but dont keep it in clipboard
 
-keymap.set("n", "<C-s>", ":w<CR>") -- in NORMAL mode save
+keymap.set("n", "ss", ":w<CR>") -- in NORMAL mode save
 keymap.set("n", "zz", ":wq<CR>") -- in NORMAL mode save and exit
+keymap.set("n", "qq", ":q<CR>") -- in NORMAL mode exit
 
 keymap.set("v", "<Tab>", ">") -- in VISUAL mode use Tab to indent lines
 
@@ -15,7 +15,6 @@ keymap.set("v", "<Tab>", ">") -- in VISUAL mode use Tab to indent lines
 
 keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
 -- Tabs:
@@ -44,28 +43,7 @@ keymap.set("n", "<leader>b", "<cmd>Telescope buffers<cr>") -- list open buffers 
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 
 -- lazygit
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
 keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>") -- toggle LazyGit
 
-vim.keymap.set("n", "gD", function()
-	local org_path = vim.api.nvim_buf_get_name(0)
-
-	-- Go to definition:
-	vim.api.nvim_command("normal gd")
-
-	-- Wait LSP server response
-	vim.wait(100, function() end)
-
-	local new_path = vim.api.nvim_buf_get_name(0)
-	if not (org_path == new_path) then
-		-- Create a new tab for the original file
-		vim.api.nvim_command("0tabnew %")
-
-		-- Restore the cursor position
-		vim.api.nvim_command("b " .. org_path)
-		vim.api.nvim_command('normal! `"')
-
-		-- Switch to the original tab
-		vim.api.nvim_command("normal! gt")
-	end
-end, bufopts)
+-- go to definition
+-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
--- .config/nvim/lua/noct/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/noct/plugins/lsp/lspconfig.lua
@@ -26,7 +26,8 @@ local on_attach = function(client, bufnr)
 	-- set keybinds
 	keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
 	keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
-	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+	--keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+	keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts) -- see definition and make edits in window
 	keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
 	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
 	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
@@ -36,6 +37,12 @@ local on_attach = function(client, bufnr)
 	keymap.set("n", "]f", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
 	keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
 	keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+	keymap.set("n", "gv", function()
+		vim.cmd([[
+vsplit
+]])
+		vim.lsp.buf.definition()
+	end, opts)
 
 	-- typescript specific keymaps (e.g. rename file and update imports)
 	if client.name == "tsserver" then
